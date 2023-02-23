0f9e5fe778d13cfec5ffd1eac89cb48d9382bf74
Syed Kabir Ali Shah <shahkabir456@gmail.com>
2023-02-23 23:06:49 +0000

    Added EVP instead of Vehicle Physics Pro

--- RacingML/Assets/Scripts/CarAgent.cs
+++ RacingML/Assets/Scripts/CarAgent.cs
@@ -4,15 +4,19 @@ using UnityEngine;
 using Unity.MLAgents; 
 using Unity.MLAgents.Actuators;
 using Unity.MLAgents.Sensors; 
+using VehiclePhysics;
+using EVP;
 
 public class CarAgent : Agent {
 
+    public VehicleController VehicleController;
     public override void OnActionReceived(ActionBuffers actions) { 
+        
         
     }
     public override void CollectObservations(VectorSensor sensor) { 
         sensor.AddObservation(transform.position); // When taking a position vector, this is comprised of 3 floats, so make sure to adjust the space size for Vector Observations by 3 instead of 1 each time a new position vector is added. 
-        sensor.AddObservation();
+        
     }
    
 } 
